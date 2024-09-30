b3f749891d986a6e5f24e7b1c75a34cb4e1f632b
Sonupalsingh <73596998+Sonupalsingh@users.noreply.github.com>
2024-09-30 16:43:40 +0530

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,41 +1,47 @@
-#!/bin/bash
 
-# Variables
-VHOST_CONFIG="/etc/httpd/conf.d/puneet.conf"
-DOCUMENT_ROOT="/var/www/puneet"
-SERVER_NAME="puneet.example.com"
-SERVER_ALIAS="puneet.com"
+echo "Enter the IP address:"
+read ip
+
+echo "Enter the webname:"
+read NAME
+
+
+VHOST_CONFIG="/etc/httpd/conf.d/"$NAME".conf"
+DOCUMENT_ROOT="/var/www/"$NAME""
+SERVER_NAME=""$NAME".example.com"
+SERVER_ALIAS=""$NAME".com"
 SSL_CERT="/etc/ssl/certs/mydomain.crt"
 SSL_KEY="/etc/ssl/private/mydomain.key"
-ACCESS_LOG="/var/log/httpd/puneet_log"
-ERROR_LOG="/var/log/httpd/puneet_error_log"
+ACCESS_LOG="/var/log/httpd/"$NAME"_log"
+ERROR_LOG="/var/log/httpd/"$NAME"_error_log"
 
 # Create Document Root Directory if it doesn't exist
 if [ ! -d "$DOCUMENT_ROOT" ]; then
-	    mkdir -p "$DOCUMENT_ROOT"
-	        echo "<html><h1>Welcome to $SERVER_NAME</h1></html>" > "$DOCUMENT_ROOT/index.html"
-		    echo "Created document root: $DOCUMENT_ROOT"
-	    else
-		        echo "Document root already exists: $DOCUMENT_ROOT"
-		fi
+   mkdir -p "$DOCUMENT_ROOT"
+   echo "<html><h1>Welcome to $SERVER_NAME</h1></html>" > "$DOCUMENT_ROOT/index.html"
+   echo "Created document root: $DOCUMENT_ROOT"
+   else
+   echo "Document root already exists: $DOCUMENT_ROOT"
+        fi
 
-		# Create Virtual Host Configuration
-		cat <<EOL > "$VHOST_CONFIG"
-<VirtualHost 192.168.0.201:80>
+                                                                                                                        # Create Virtual Host Configuration
+
+           cat <<EOL > "$VHOST_CONFIG"
+<VirtualHost $ip:80>
     ServerAdmin root@node.example.com
     DocumentRoot $DOCUMENT_ROOT
     DirectoryIndex index.html
     ServerName $SERVER_NAME
     ServerAlias $SERVER_ALIAS
     #Redirect permanent / https://$SERVER_NAME/
-
+    
     #SSLEngine on
     #SSLCertificateFile $SSL_CERT
     #SSLCertificateKeyFile $SSL_KEY
     # If you have an intermediate certificate, add the following line
     # SSLCertificateChainFile /etc/ssl/certs/intermediate.crt
 
-    CustomLog "$ACCESS_LOG" combined
+        CustomLog "$ACCESS_LOG" combined
     ErrorLog "$ERROR_LOG"
 </VirtualHost>
 
@@ -46,11 +52,11 @@ EOL
 
 echo "Virtual host configuration created at $VHOST_CONFIG"
 
+echo "$ip     $SERVER_NAME    $SERVER_ALIAS  " >> /etc/hosts
+
 # Restart Apache service
 systemctl restart httpd
 
 # Check Apache status
 systemctl status httpd
-
 echo "Apache service restarted."
-
